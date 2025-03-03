217926951f0e7f13b7dfdd62b607b8ccf319b767
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2025-03-02 18:14:46 -0600

    Commit - 20250302 - Confident Coders TeamProject01
    * Added ability to add games to the database from the main landing page
    + Added game_details.html to see the details of a particular game
    * Changed index.html to have a section for adding a game to the database

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 
-# Function to fetch all games from the database
+# This function will get a list of all games so that it can be displayed on the main landing page:
 def get_all_games():
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
     cursor = conn.cursor()
@@ -12,12 +12,16 @@ def get_all_games():
     conn.close()
     return games
 
-@app.route('/')
-def hello_world():  # put application's code here
-    games = get_all_games()
-    return render_template('index.html', games=games)
+# This function will get a specific games details:
+def get_game_details(game_id):
+    conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT game_name, description FROM games WHERE game_id = ?", (game_id,))
+    game = cursor.fetchone()
+    conn.close()
+    return game
 
-
+# This function will get a games leaderboard details:
 def get_leaderboard_data(game_name):
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
     cursor = conn.cursor()
@@ -32,10 +36,55 @@ def get_leaderboard_data(game_name):
     conn.close()
     return rows
 
+# This function will add a new game to the database:
+def add_game_to_db(game_name, description):
+    conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO games (game_name, description) VALUES (?, ?)", (game_name, description))
+    conn.commit()
+    conn.close()
+
+
+## Routes:
+
+# Main landing page route with GET and POST to allow for adding games to the database:
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # Display all games
+    games = get_all_games()  # Fetch all games from the database
+    return render_template('index.html', games=games)
+
+
+# Leaderboard for an individual game:
 @app.route('/leaderboard/<game_name>')
 def leaderboard(game_name):
     data = get_leaderboard_data(game_name)
     return render_template('leaderboard.html', game_name=game_name, data=data)
+
+# Route to a games details page:
+# Game Details page
+@app.route('/game/<int:game_id>')
+def game_details(game_id):
+    game = get_game_details(game_id)
+    if game:
+        return render_template('game_details.html', game=game)
+    else:
+        return "Game not found", 404
+
+@app.route('/add_game', methods=['GET', 'POST'])
+def add_game():
+    if request.method == 'POST':
+        # Get form data for the new game
+        game_name = request.form['game_name']
+        description = request.form['description']
+
+        # Insert new game into the database
+        add_game_to_db(game_name, description)
+
+        # Redirect to the homepage to show the updated list of games
+        return redirect(url_for('home'))
+    else:
+        print("test")
 
 
 if __name__ == '__main__':
