e79a6180ab6416dfba43a749e4d6155e690a678f
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2025-04-13 20:03:28 -0500

    Commit - 20250413 - MM
    * Performed static analysis on the following:
    
    .\app.py:9:1: E303 too many blank lines (4)
    .\app.py:9:80: E501 line too long (120 > 79 characters)
    .\app.py:10:1: E266 too many leading '#' for block comment
    .\app.py:10:80: E501 line too long (120 > 79 characters)
    .\app.py:11:80: E501 line too long (120 > 79 characters)
    .\app.py:16:1: E303 too many blank lines (4)
    .\app.py:16:80: E501 line too long (98 > 79 characters)
    .\app.py:17:1: E302 expected 2 blank lines, found 4
    .\app.py:26:1: E302 expected 2 blank lines, found 1
    .\app.py:29:80: E501 line too long (92 > 79 characters)
    .\app.py:35:1: E302 expected 2 blank lines, found 1
    .\app.py:57:1: E302 expected 2 blank lines, found 1
    .\app.py:68:1: E302 expected 2 blank lines, found 1
    .\app.py:71:80: E501 line too long (104 > 79 characters)
    .\app.py:76:1: E302 expected 2 blank lines, found 1
    .\app.py:86:1: E303 too many blank lines (4)
    .\app.py:86:80: E501 line too long (120 > 79 characters)
    .\app.py:87:1: E266 too many leading '#' for block comment
    .\app.py:87:80: E501 line too long (120 > 79 characters)
    .\app.py:88:80: E501 line too long (120 > 79 characters)
    .\app.py:93:1: E303 too many blank lines (4)
    .\app.py:93:80: E501 line too long (86 > 79 characters)
    .\app.py:94:1: E302 expected 2 blank lines, found 4
    .\app.py:121:80: E501 line too long (97 > 79 characters)
    .\app.py:124:1: E302 expected 2 blank lines, found 1
    .\app.py:133:1: E302 expected 2 blank lines, found 1
    .\app.py:147:1: E302 expected 2 blank lines, found 1
    
    All have been fixed

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ import sqlite3
 app = Flask(__name__)
 
 
+##############################################################################
+#  Functions:                                                                #
+##############################################################################
 
 
-########################################################################################################################
-##  Functions:                                                                                                        ##
-########################################################################################################################
-
-
-
-
-# This function will get a list of all games so that it can be displayed on the main landing page:
+# This function will get a list of all games so that it can be displayed on
+# the main landing page:
 def get_all_games():
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
     cursor = conn.cursor()
@@ -22,14 +19,20 @@ def get_all_games():
     conn.close()
     return games
 
+
 # This function will get a specific games details:
 def get_game_details(game_id):
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
     cursor = conn.cursor()
-    cursor.execute("SELECT game_name, description FROM games WHERE game_id = ?", (game_id,))
+    cursor.execute("""
+        SELECT game_name, description
+        FROM games
+        WHERE game_id = ?
+    """, (game_id,))
     game = cursor.fetchone()
     conn.close()
     return game
+
 
 # This function will get a games leaderboard details:
 def get_leaderboard_data(game_name):
@@ -53,6 +56,7 @@ def get_leaderboard_data(game_name):
 
     return leaderboard_data
 
+
 # Add user score to the leaderboard:
 def add_leaderboard_entry(game_name, player_name, score, video_submission):
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
@@ -64,13 +68,18 @@ def add_leaderboard_entry(game_name, player_name, score, video_submission):
     conn.commit()
     conn.close()
 
+
 # This function will add a new game to the database:
 def add_game_to_db(game_name, description):
     conn = sqlite3.connect('MSD-P01-LeaderBoard.sqlite')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO games (game_name, description) VALUES (?, ?)", (game_name, description))
+    cursor.execute("""
+        INSERT INTO games (game_name, description)
+        VALUES (?, ?)
+    """, (game_name, description))
     conn.commit()
     conn.close()
+
 
 # This function will remove a game from the database:
 def delete_game_from_db(game_id):
@@ -81,16 +90,13 @@ def delete_game_from_db(game_id):
     conn.close()
 
 
+##############################################################################
+#  Routes:                                                                   #
+##############################################################################
 
 
-########################################################################################################################
-##  Routes:                                                                                                           ##
-########################################################################################################################
-
-
-
-
-# Main landing page route with GET and POST to allow for adding games to the database:
+# Main landing page route with GET and POST to allow for adding games to
+# the database:
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # Display all games
@@ -118,7 +124,10 @@ def leaderboard(game_name):
 
     # GET request: display the leaderboard for the specific game
     leaderboard_data = get_leaderboard_data(game_name)
-    return render_template('leaderboard.html', game_name=game_name, leaderboard=leaderboard_data)
+    return render_template('leaderboard.html',
+                           game_name=game_name,
+                           leaderboard=leaderboard_data)
+
 
 # Route to a games details page:
 @app.route('/game/<int:game_id>')
@@ -128,6 +137,7 @@ def game_details(game_id):
         return render_template('game_details.html', game=game)
     else:
         return "Game not found", 404
+
 
 # Add the game to the database:
 @app.route('/add_game', methods=['GET', 'POST'])
@@ -142,6 +152,7 @@ def add_game():
 
         # Redirect to the homepage to show the updated list of games
         return redirect(url_for('home'))
+
 
 # Delete the game listed in the database:
 @app.route('/delete_game/<int:game_id>', methods=['POST'])
